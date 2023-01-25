23f020e5020282676e95a895a3e88398e6d88380
mehedithedev <mehedihasan2882@icloud.com>
2023-01-25 20:36:16 +0600

    initial commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout: 3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
